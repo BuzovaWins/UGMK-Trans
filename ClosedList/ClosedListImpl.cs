0c3ddf6f5715422a416de2270e917bfdcd34f92c
Салих Габитов <salihgabitov@yandex.ru>
2023-03-10 01:32:55 +0300

    code

---
+++ ClosedList/ClosedListImpl.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClosedList
+{
+    public class ClosedListImpl<T> : IClosedList<T>
+    {
+        private int currentIndex;
+        private List<T> list;
+
+        public ClosedListImpl()
+        {
+            currentIndex = 0;
+            list = new List<T>();
+            
+            HeadReached += ClosedList_HeadReached;
+
+            if (File.Exists("test.txt"))
+                File.Delete("test.txt");
+        }
+
+        public T Head => this[0];
+
+        public T Current { get { return list[currentIndex]; } set { } }
+
+        public T Previous
+        {
+            get
+            {
+                if (list.Count == 0)
+                    throw new Exception("Closed list is empty");
+                if (currentIndex == 0)
+                    currentIndex = list.Count;
+                return this[--currentIndex];
+            }
+        }
+
+        public T Next
+        {
+            get
+            {
+                if (list.Count == 0)
+                    throw new Exception("Closed list is empty");
+                if (currentIndex == list.Count - 1)
+                    currentIndex = -1;
+                return this[++currentIndex];
+            }
+        }
+
+        public int Count => this.Count;
+
+        public bool IsReadOnly => false;
+
+        public T this[int index] { get => list[index]; set => list[index] = value; }
+
+        public event EventHandler<T> HeadReached;
+
+        public void Add(T item)
+        {
+            list.Add(item);
+        }
+
+        public void Clear()
+        {
+            list.Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            return list.Contains(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            list.CopyTo(array, arrayIndex);
+        }
+
+        public int IndexOf(T item)
+        {
+            return list.IndexOf(item);
+        }
+
+        public void Insert(int index, T item)
+        {
+            list.Insert(index, item);
+        }
+
+        public void MoveBack(int step = 1)
+        {
+            for (int index = 0; index < step; index++)
+            {
+                Current = Previous;
+                if (currentIndex == 0)
+                    HeadReached?.Invoke(this, this.Current);
+            }
+        }
+
+        public void MoveNext(int step = 1)
+        {
+            for (int index = 0; index < step; index++)
+            {
+                Current = Next;
+                if (currentIndex == 0)
+                    HeadReached?.Invoke(this, this.Current);
+            }
+        }
+
+        public bool Remove(T item)
+        {
+            return list.Remove(item);
+        }
+
+        public void RemoveAt(int index)
+        {
+            list.RemoveAt(index);
+        }
+
+        private void ClosedList_HeadReached(object sender, T e)
+        {
+            StreamWriter f = new StreamWriter("test.txt");
+            f.WriteLine("We meet head item");
+            f.Close();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
---
+++ TestClosedList/TestClosedList.cs
@@ -0,0 +1,152 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ClosedList;
+using System.IO;
+
+namespace TestClosedList
+{
+    public class TestClosedList
+    {
+        public string getTestFileOutput()
+        {
+            var fileName = "test.txt";
+
+            if (!File.Exists(fileName))
+                return "";
+
+            var sr = new StreamReader(fileName);
+
+            string content = sr.ReadToEnd();
+            return content;
+        }
+
+        [Test]
+        public void MoveNextOnEmptyList_ThenException()
+        {
+            var list = new ClosedListImpl<string>();            
+
+            Assert.Throws<Exception>(() => list.MoveNext());
+        }
+
+        [Test]
+        public void MoveNextOnEmptyList_ThenZeroHeadReached()
+        {
+            MoveNextOnEmptyList_ThenException();
+
+            Assert.AreEqual("", getTestFileOutput());
+        }
+
+        [Test]
+        public void MoveBackOnEmptyList_ThenException()
+        {
+            var list = new ClosedListImpl<string>();
+
+            Assert.Throws<Exception>(() => list.MoveBack());
+        }
+
+        [Test]
+        public void MoveBackOnEmptyList_ThenZeroHeadReached()
+        {
+            MoveBackOnEmptyList_ThenException();
+
+            Assert.AreEqual("", getTestFileOutput());
+        }
+
+        [Test]
+        public void CurrentOn1ItemList_ThenFirstValue()
+        {
+            var list = new ClosedListImpl<string>() { "1" };
+
+            Assert.AreEqual("1", list.Current);
+        }
+
+        [Test]
+        public void CurrentOn1ItemList_ThenZeroHeadReached()
+        {
+            var list = new ClosedListImpl<string>() { "1" };
+            var c = list.Current;
+
+            Assert.AreEqual("", getTestFileOutput());
+        }
+
+        [Test]
+        public void Move1NextOn1ItemList_ThenSameValue()
+        {
+            var list = new ClosedListImpl<string>() { "1" };
+            list.MoveNext();
+
+            Assert.AreEqual("1", list.Current);
+        }
+
+        [Test]
+        public void Move1NextOn1ItemList_Then1HeadReached()
+        {
+            var list = new ClosedListImpl<string>() { "1" };
+                list.MoveNext();
+
+            Assert.AreEqual("We meet head item\r\n", getTestFileOutput());
+        }
+
+        [Test]
+        public void Move1BackOn1ItemList_ThenSameValue()
+        {
+            var list = new ClosedListImpl<string>() { "1" };
+            list.MoveBack();
+
+            Assert.AreEqual("1", list.Current);
+        }
+
+        [Test]
+        public void Move1BackOn1ItemList_Then1HeadReached()
+        {
+            var list = new ClosedListImpl<string>() { "1" };
+            list.MoveBack();
+
+            Assert.AreEqual("We meet head item\r\n", getTestFileOutput());
+        }
+
+        [Test]
+        public void Move2NextOn1ItemList_ThenSameValue()
+        {
+            var list = new ClosedListImpl<string>() { "1" };
+            list.MoveNext();
+            list.MoveNext();
+
+            Assert.AreEqual("1", list.Current);
+        }
+
+        [Test]
+        public void Move2BackOn1ItemList_ThenSameValue()
+        {
+            var list = new ClosedListImpl<string>() { "1" };
+            list.MoveBack();
+            list.MoveBack();
+
+            Assert.AreEqual("1", list.Current);
+        }
+
+        [Test]
+        public void Move2NextOn1ItemList_Then2HeadReached()
+        {
+            var list = new ClosedListImpl<string>() { "1" };
+            list.MoveNext();
+            list.MoveNext();
+
+            Assert.AreEqual("We meet head item\r\nWe meet head item\r\n", getTestFileOutput());
+        }
+
+        [Test]
+        public void Move2BackOn1ItemList_Then2HeadReached()
+        {
+            var list = new ClosedListImpl<string>() { "1" };
+            list.MoveBack();
+            list.MoveBack();
+
+            Assert.AreEqual("We meet head item\r\nWe meet head item\r\n", getTestFileOutput());
+        }
+    }
+}
